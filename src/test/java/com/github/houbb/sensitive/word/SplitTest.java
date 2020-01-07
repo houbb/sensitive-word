a85152061e6260bfcc31631f6c4255a5473312c9
binbin.hou <1060732496@qq.com>
2020-01-07 17:42:05 +0800

    [Feature] add for new

--- src/test/java/com/github/houbb/sensitive/word/SplitTest.java
+++ src/test/java/com/github/houbb/sensitive/word/SplitTest.java
@@ -1,10 +1,15 @@
 package com.github.houbb.sensitive.word;
 
+import com.github.houbb.heaven.support.filter.IFilter;
 import com.github.houbb.heaven.util.io.FileUtil;
 import com.github.houbb.heaven.util.lang.StringUtil;
+import com.github.houbb.heaven.util.util.CharsetUtil;
+import com.github.houbb.heaven.util.util.CollectionUtil;
+import com.github.houbb.sensitive.word.util.CharsetUtils;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -23,6 +28,41 @@ public class SplitTest {
 
         List<String> words = StringUtil.splitToList(string, "\\|");
         FileUtil.write(targetFile, words);
+    }
+
+    @Test
+    @Ignore
+    public void trimTest() {
+        final String source = "D:\\github\\sensitive-word\\src\\main\\resources\\dict.txt";
+        List<String> lines = FileUtil.readAllLines(source);
+        List<String> trimLines = CollectionUtil.distinct(CollectionUtil.trimCollection(lines));
+
+        final String target = "D:\\github\\sensitive-word\\src\\main\\resources\\dict.txt";
+        FileUtil.write(target, trimLines);
+    }
+
+    private List<String> distinctLines(final String name) {
+        final String dir = "D:\\github\\sensitive-word\\src\\main\\resources\\";
+        final String path = dir + name;
+        List<String> lines = FileUtil.readAllLines(path);
+        return CollectionUtil.distinct(lines);
+    }
+
+    @Test
+    public void stopWordTest() {
+        final String source = "D:\\github\\sensitive-word\\src\\main\\resources\\stopword.txt";
+        final String target = "D:\\github\\sensitive-word\\src\\main\\resources\\stopword_chars.txt";
+
+        final List<String> lines = FileUtil.readAllLines(source);
+
+        List<String> resultList = CollectionUtil.distinct(CollectionUtil.filterList(lines, new IFilter<String>() {
+            @Override
+            public boolean filter(String s) {
+                return CharsetUtils.isContainsChinese(s);
+            }
+        }));
+        Collections.sort(resultList);
+        FileUtil.write(target, resultList);
     }
 
 }
